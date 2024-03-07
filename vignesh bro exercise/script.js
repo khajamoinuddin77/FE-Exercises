5dfed3b408efa99289282ae8030193f80f3b3ea0
khajamoinuddin77 <104656971+khajamoinuddin77@users.noreply.github.com>
2024-03-07 08:03:00 +0530

    Add files via upload

---
+++ vignesh bro exercise/script.js
@@ -0,0 +1,34 @@
+function calculate() {
+    var num1 = parseFloat(document.getElementById('num1').value);
+    var num2 = parseFloat(document.getElementById('num2').value);
+    var operation = document.getElementById('operation').value;
+
+    var result;
+
+    switch (operation) {
+        case 'add':
+            result = num1 + num2;
+            break;
+        case 'subtract':
+            result = num1 - num2;
+            break;
+        case 'multiply':
+            result = num1 * num2;
+            break;
+        case 'divide':
+            if (num2 !== 0) {
+                result = num1 / num2;
+            } else {
+                alert("Cannot divide by zero!");
+                return;
+            }
+            break;
+        default:
+            alert("Invalid operation");
+            return;
+    }
+
+    var resultWindow = window.open("", "Result", "width=150,height=100");
+    resultWindow.document.write("<h2>Result:</h2>");
+    resultWindow.document.write("<p>" + result + "</p>");
+}
